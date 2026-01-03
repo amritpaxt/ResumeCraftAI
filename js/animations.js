aec60159ec5418f837cc74139ce976a0dc2db3e5
Amrita Yadav <yadamrita936@gmail.com>
2026-01-03 14:01:34 +0530

    Initial commit - frontend MVP

---
+++ js/animations.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.style.boxShadow = "0 20px 40px rgba(0,0,0,0.4)";
+  });
+  card.addEventListener('mouseleave', () => {
+    card.style.boxShadow = "none";
+  });
+});
